46c8047134813fe621ec01d951d55b51ce0cb1d5
Wanz27 <158497133+Wanz27@users.noreply.github.com>
2025-09-24 22:07:07 +0700

    Update medicationModel.js

--- src/models/medicationModel.js
+++ src/models/medicationModel.js
@@ -1,27 +1,40 @@
 import { supabase } from "../config/supabaseClient.js";
 
 export const MedicationModel = {
-    async getAll() {
-        const { data, error } = await supabase
-            .from("medications")
-            .select(
-                "id, sku, name, description, price, quantity, category_id, supplier_id"
-            );
-
-        if (error) throw error;
-        return data;
+    async getAllWithPagination(page, limit) { 
+        const from = (page - 1) * limit; 
+        const to = from + limit - 1; 
+        const { data, error, count } = await supabase 
+            .from("medications") 
+            .select( 
+                "id, sku, name, description, price, quantity, category_id, supplier_id", 
+                { count: "exact" } 
+            ) 
+            .range(from, to); 
+        if (error) throw error; 
+        return { 
+            data, 
+            total: count, 
+            page, 
+            limit, 
+            totalPages: Math.ceil(count / limit), 
+        }; 
     },
+
 
     async getById(id) {
         const { data, error } = await supabase
             .from("medications")
-            .select(
-                `
-                id, sku, name, description, price, quantity,
-                categories ( id, name ),
-                suppliers ( id, name, email, phone ),
-                `
-            )
+            .select(`
+                id,
+                sku,
+                name,
+                description,
+                price,
+                quantity,
+                categories(id, name),
+                suppliers(id, name, email, phone)
+            `)
             .eq("id", id)
             .single();
         if (error) throw error;
@@ -51,5 +64,13 @@ export const MedicationModel = {
         const { error } = await supabase.from("medications").delete().eq("id", id);
         if (error) throw error;
         return { success: true };
+    },
+
+    async getTotal() {
+        const { count, error } = await supabase
+            .from("medications")
+            .select("id", { count: "exact", head: true }); // hanya ambil count
+        if (error) throw error;
+        return count;
     },
 };
